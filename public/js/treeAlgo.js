76ba32524cbbc2495249d80bf66c599916e3300c
Colin Graydon <colingraydon@gmail.com>
2022-09-11 12:58:17 -0400

    rereshing tree input works now

--- public/js/treeAlgo.js
+++ public/js/treeAlgo.js
@@ -1,68 +1,23 @@
 
-
+//sets the container for the nodes
 const container = document.querySelector(".tree-container");
-console.log("opened script file")
 
-document.addEventListener('DOMContentLoaded', function randomizeInput(num = 10) {
-
-    console.log("entered event listener")
-    const  arr = []
-    for (let i = 0; i < num; i += 1) {
-
-        console.log("entered for loop")
-        var value = Math.floor(Math.random() * 100)
-        
-        if (arr.includes(value)) {
-            console.log("entered include block")
-            while (arr.includes(value)) {
-                value = Math.floor(Math.random() * 100)
-            }
-        }
-        arr.push(value)
-
-
-        var myDiv = document.getElementById("tree-values")
-        var strong = document.createElement("a")
-        myDiv.classList.add("tree-values")
-        strong.classList.add("tree-values")
-        strong.innerHTML = value
-        strong.textContent = value + ' '.repeat(5)
-        myDiv.appendChild(strong)
-        
-
-    }
-    
-
-
-
-    let vals = document.querySelectorAll(".tree-values")
-    console.log("vals.length is" + vals.length)
-    for (var i = 1; i < vals.length; i += 1) {
-
-        console.log("vals[i] is" + vals[i].innerHTML)
-        //console.log("tempval is" + tempVal)
-    }
-
-});
-
+//randomizes the 10 vals used to construct the string, called on page load
 function randomizeInput(num = 10) {
-
-    console.log("entered function")
+    
     var arr = []
     for (let i = 0; i < num; i += 1) {
 
-        console.log("entered for loop")
         var value = Math.floor(Math.random() * 100)
         
         if (arr.includes(value)) {
-            console.log("entered include block")
             while (arr.includes(value)) {
                 value = Math.floor(Math.random() * 100)
             }
         }
         arr.push(value)
 
-
+        //gets all elements from tree values. creates an element, adds it to the calss list, sets values, appends it
         var myDiv = document.getElementById("tree-values")
         var strong = document.createElement("a")
         myDiv.classList.add("tree-values")
@@ -70,33 +25,40 @@ function randomizeInput(num = 10) {
         strong.innerHTML = value
         strong.textContent = value + ' '.repeat(5)
         myDiv.appendChild(strong)
+    }
+}
+
+//used to get the tree vals and then randomize them again
+function refreshInput(num = 10) {
+
+
+    //creates an arr to make sure that no values are repeated
+    var arr = []
+    for (let i = 0; i < num; i += 1) {
         
-
+        var value = Math.floor(Math.random() * 100)
+        
+        if (arr.includes(value)) {
+            while (arr.includes(value)) {
+                value = Math.floor(Math.random() * 100)
+            }
+        }
+        arr.push(value)
     }
     
 
-
-
+    //uses arr values to modify the innerhtlm and text content of the elements
     let vals = document.querySelectorAll(".tree-values")
-    console.log("vals.length is" + vals.length)
-    for (var i = 1; i < vals.length; i += 1) {
 
-        console.log("vals[i] is" + vals[i].innerHTML)
-        //console.log("tempval is" + tempVal)
+    for (let i = 0; i < arr.length; i += 1) {
+
+        tempValue = arr[i]
+        vals[i+1].innerHTML = tempValue
+        vals[i+1].textContent = tempValue + ' '
     }
 
 }
-
     
-function testDivFunction() {
-
-    let vals = document.querySelectorAll(".tree-values")
-    for (var i = 0; i < vals.length; i += 1) {
-
-        tempVal = parseInt(vals[i].childNodes[0].innerHTML)
-        console.log("tempval is" + tempVal)
-    }
-}
 //sets constants for accessing child value in the children array, to keep it easy to remember
 var l = 0
 var r = 1
