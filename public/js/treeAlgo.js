ee7f52afb52a835b8da1ef7fb3625fe37e9cf65e
Colin Graydon <colingraydon@gmail.com>
2022-10-01 01:30:43 -0400

    changed to BST, implemented levels

--- public/js/treeAlgo.js
+++ public/js/treeAlgo.js
@@ -10,33 +10,36 @@ class Node{
 
     constructor(value) {
         this.value = value
+        this.level = 0
         this.left = null
         this.right = null
     }
 }
 
-
-function inOrderWrapper() {
-
-}
 class Tree{
 
     constructor() { 
         this.root = null
+        this.levels = [0,0,0,0,0,0,0]
     }
 
-    addNode(val) {
+    addNode(tempval) {
         
+        let val = Number(tempval)
         let newNode = new Node(val)
 
         const searchTree = node => {
 
             if (this.root === null) {
+                let temp = newNode.level
+                this.levels[temp] = this.levels[temp] +1
                 this.root = newNode
             }
             else if (val < node.value) {
 
+                newNode.level++
                 if (!node.left) {
+                    this.levels[newNode.level] = this.levels[newNode.level] + 1
                     node.left = newNode
                 }
 
@@ -46,7 +49,9 @@ class Tree{
             }
             else if (val > node.value) {
 
+                newNode.level++
                 if (!node.right) {
+                    this.levels[newNode.level] = this.levels[newNode.level] + 1
                     node.right = newNode
                 }
                 else {
@@ -96,6 +101,13 @@ class Tree{
         return result
     }
 
+
+    checkLevels() {
+
+        for (let i = 0; i < this.levels.length; i ++) {
+            console.log("level", i , "contains", this.levels[i])
+        }
+    }
     // 
 }
 
@@ -105,16 +117,72 @@ class Tree{
 const container = document.querySelector(".tree-container");
 
 //randomizes the 10 vals used to construct the string, called on page load
-function randomizeInput(num = 10) {
+function randomizeInput(num = 7) {
     
     var arr = []
     for (let i = 0; i < num; i += 1) {
 
-        var value = Math.floor(Math.random() * 100)
+        var value = 0
+        if (i == 0) {
+            value = Math.floor(Math.random() * 20) + 40
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 20) + 40
+                }
+            }
+        }
         
-        if (arr.includes(value)) {
-            while (arr.includes(value)) {
-                value = Math.floor(Math.random() * 100)
+        if (i == 1) {
+            value = Math.floor(Math.random() * 10) + 15
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 10) + 15
+                }
+            }
+        }
+
+        if (i == 2) {
+            value = Math.floor(Math.random() * 10) + 75
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 10) + 75
+                }
+            }
+        }
+
+        if (i == 3) {
+            value = Math.floor(Math.random() * 14)
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 14)
+                }
+            }
+        }
+
+        if (i == 4) {
+            value = Math.floor(Math.random() * 13) + 26
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 26
+                }
+            }
+        }
+
+        if (i == 5) {
+            value = Math.floor(Math.random() * 13) + 61
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 61
+                }
+            }
+        }
+
+        if (i == 6) {
+            value = Math.floor(Math.random() * 13) + 86
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 86
+                }
             }
         }
         arr.push(value)
@@ -131,18 +199,74 @@ function randomizeInput(num = 10) {
 }
 
 //used to get the tree vals and then randomize them again
-function refreshInput(num = 10) {
+function refreshInput(num = 7) {
 
 
     //creates an arr to make sure that no values are repeated
     var arr = []
     for (let i = 0; i < num; i += 1) {
         
-        var value = Math.floor(Math.random() * 100)
+        var value = 0
+        if (i == 0) {
+            value = Math.floor(Math.random() * 20) + 40
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 20) + 40
+                }
+            }
+        }
         
-        if (arr.includes(value)) {
-            while (arr.includes(value)) {
-                value = Math.floor(Math.random() * 100)
+        if (i == 1) {
+            value = Math.floor(Math.random() * 10) + 15
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 10) + 15
+                }
+            }
+        }
+
+        if (i == 2) {
+            value = Math.floor(Math.random() * 10) + 75
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 10) + 75
+                }
+            }
+        }
+
+        if (i == 3) {
+            value = Math.floor(Math.random() * 14)
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 14)
+                }
+            }
+        }
+
+        if (i == 4) {
+            value = Math.floor(Math.random() * 13) + 26
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 26
+                }
+            }
+        }
+
+        if (i == 5) {
+            value = Math.floor(Math.random() * 13) + 61
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 61
+                }
+            }
+        }
+
+        if (i == 6) {
+            value = Math.floor(Math.random() * 13) + 86
+            if (arr.includes(value)) {
+                while (arr.includes(value)) {
+                    value = Math.floor(Math.random() * 13) + 86
+                }
             }
         }
         arr.push(value)
@@ -161,7 +285,9 @@ function refreshInput(num = 10) {
 
 }
 
-
+function drawTree() {
+    
+}
 
 function generateTree() {
 
@@ -186,8 +312,8 @@ function inOrderWrapper() {
     
     let bst = generateTree()
     let tempRoot = bst.root
-    console.log("root is", tempRoot)
     let result = inOrder(tempRoot)
+    bst.checkLevels()
     return result
 }
 
@@ -195,12 +321,8 @@ function inOrderWrapper() {
 function inOrder(node) {
 
     let result = []
-    
-    console.log("called inOrder")
-    console.log("left node is", node.left)
     if (node.left) inOrder(node.left)
     result.push(node.value)
-    console.log(node.value, "= node.value inOrder")
     if (node.right) inOrder(node.right) 
     return result   
 }
@@ -209,7 +331,6 @@ function preOrderWrapper() {
     
     let bst = generateTree()
     let tempRoot = bst.root
-    console.log("root is", tempRoot)
     let result = preOrder(tempRoot)
     return result
 }
@@ -218,12 +339,8 @@ function preOrderWrapper() {
 function preOrder(node) {
 
     let result = []
-    
-    console.log("called preOrder")
-    console.log("left node is", node.left)
     result.push(node.value)
     if (node.left) preOrder(node.left)
-    console.log(node.value, "= node.value preOrder")
     if (node.right) preOrder(node.right) 
     return result   
 }
@@ -233,7 +350,6 @@ function postOrderWrapper() {
     
     let bst = generateTree()
     let tempRoot = bst.root
-    console.log("root is", tempRoot)
     let result = postOrder(tempRoot)
     return result
 }
@@ -242,13 +358,12 @@ function postOrderWrapper() {
 function postOrder(node) {
 
     let result = []
-    
-    console.log("called postOrder")
-    console.log("left node is", node.left)
     if (node.left) preOrder(node.left)
     if (node.right) preOrder(node.right) 
     result.push(node.value)
     return result   
 }
+
+
 
 
